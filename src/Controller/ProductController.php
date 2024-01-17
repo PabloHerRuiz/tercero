8c350b46fb81566d698c706161ec4c4bcb731302
PabloHerRuiz <pherrui680@g.educaand.es>
2024-01-17 10:04:08 +0100

    formulario

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Producto;
 use App\Entity\Categoria;
+
+use App\Form\ProductoType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -45,6 +48,31 @@ class ProductController extends AbstractController
         ]);
     }
 
+    #[Route('/producto/formulario', name: 'formulario')]
+    public function formulario(Request $request,EntityManagerInterface $entityManager): Response
+    {
+
+        $producto = new Producto();
+
+        $form = $this->createForm(ProductoType::class, $producto);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $producto = $form->getData();
+
+            $entityManager->persist($producto);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('product/prueba.html.twig', [
+            'form' => $form->createView(),
+            // 'form' => $form,
+        ]);
+    }
+
     #[Route('/producto/{categoria}', name: 'listado_prod')]
     public function listProd($categoria,EntityManagerInterface $entityManager): Response
     {
@@ -57,4 +85,5 @@ class ProductController extends AbstractController
             "producto" => $producto,
         ]);
     }
+    
 }
---
+++ src/Form/ProductoType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categoria;
+use App\Entity\Producto;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Nombre')
+            ->add('Cantidad')
+            ->add('Precio')
+            ->add('Descripcion')
+            ->add('Categoria', EntityType::class, [
+                'class' => Categoria::class,
+                // 'choice_label' => 'nombre',
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Producto::class,
+        ]);
+    }
+}
